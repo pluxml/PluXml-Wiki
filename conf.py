05a79292eec84a9c4f83ed21aea36f4da151a55c
P3ter <contact@p3ter.fr>
2021-01-03 00:20:38 +0100

    add furo to extension list

--- conf.py
+++ conf.py
@@ -24,15 +24,15 @@ author = 'Pedro CADETE'
 # The full version, including alpha/beta/rc tags
 release = '5.8'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'recommonmark'
-]
+extensions = {
+    'recommonmark',
+    'furo'
+}
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -53,7 +53,6 @@ language = 'fr'
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'README.md']
-
 
 # -- Options for HTML output -------------------------------------------------
 
