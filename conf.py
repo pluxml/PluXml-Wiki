cd892036defafcf50cd40f2b1a25c7a2890fb0fa
Pedro CADETE <contact@p3ter.fr>
2021-11-23 20:58:13 +0100

    fix furo theme

--- conf.py
+++ conf.py
@@ -29,14 +29,12 @@ release = '5.8'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = {
-    'recommonmark',
-    'furo'
-}
+#extensions = {
+#    'furo'
+#}
 
 source_suffix = {
     '.rst': 'restructuredtext',
-    '.md': 'markdown'
 }
 
 # Add any paths that contain templates here, relative to this directory.
@@ -61,11 +59,11 @@ exclude_patterns = ['_build', 'README.md']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'furo'
+html_theme = 'furo'
 
-#html_theme_options = {
-#    "announcement": 'PluXml',
-#}
+html_theme_options = {
+    "announcement": 'PluXml',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
